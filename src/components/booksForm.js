be9bf35d88a9d9f2482e8a1fc6fe6acba3453f2d
Ioannis Kousis <bonobo@otenet.gr>
2020-09-18 19:43:35 +0300

    fixed eslint errors

--- src/components/booksForm.js
+++ src/components/booksForm.js
@@ -22,7 +22,7 @@ export default class BooksForm extends React.Component {
 
     return (
       <div className="BooksForm">
-        <input name="title" id="title"/>
+        <input name="title" id="title" />
         <select name="category" id="category">
           {categoryOptions}
         </select>

13c1c95af3f49c02dfb9b058210333f25ddc17f8
Ioannis Kousis <bonobo@otenet.gr>
2020-09-20 12:21:31 +0300

    refactored main index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers/index';
-import { createBook, removeBook } from './actions/index';
-import './index.css';
 import App from './components/App';
+import './index.css';
 import booksSeed from './booksSeed';
 
-const store = createStore(rootReducer);
-
-const mapStateToProps = () => ({ books: booksSeed });
-
-const mapDispatchToProps = dispatch => ({
-  submitNewBook: newBook => {
-    dispatch(createBook(newBook));
-  },
-  removeBook: book => {
-    dispatch(removeBook(book));
-  },
-});
-
-const Container = connect(mapStateToProps, mapDispatchToProps)(App);
+const store = createStore(rootReducer, { books: booksSeed });
 
 const AppWrapper = () => (
   <Provider store={store}>
-    <Container />
+    <App />
   </Provider>
 );
 
